84c83d2a8d96e759a8a7d45bcc59273894db22cf
Burchell <jlkaline@me.com>
2021-05-02 15:00:10 -0400

    updates to js

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,9 @@ const FILES_TO_CACHE = [
     '/index.js',
     '/db.js',
     '/manifest.json',
-    '/css/styles.css'
+    '/css/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
   ];
 
 
