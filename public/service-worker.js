5d30791c219452095770f0df53fe35213916c7ea
Burchell <jlkaline@me.com>
2021-05-05 19:47:55 -0400

    updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,62 +4,69 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/index.js',
+    '/style.css',
     '/db.js',
     '/manifest.json',
-    '/css/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
   ];
 
 
-  const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  //install
+  self.addEventListener("install", function (evt) {
+      evt.waitUntil(
+          caches.open(CACHE_NAME).then(cache => {
+              console.log("pre-cache successful");
+              return cache.addAll(FILES_TO_CACHE);
           })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
+      );
+      self.skipWaiting();
+  });
+  
+  //activate
+  self.addEventListener("activate", function (evt) {
+      evt.waitUntil(
+          caches.keys().then(keylist => {
+              return Promise.all(
+                  keylist.map(key => {
+                      if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                          console.log("removing old cache data", key);
+                          return caches.delete(key);
+                      }
+                  })
+              );
+          })
+      );
+      self.clients.claim();
+  });
+  
+  //fetch
+  self.addEventListener("fetch", function (evt) {
+      if (evt.request.url.includes("/api/")) {
+          evt.respondWith(
+              caches.open(DATA_CACHE_NAME).then(cache => {
+                  return fetch(evt.request)
+                      .then(response => {
+                          if (response.status === 200) {
+                              cache.put(evt.request.url, response.clone());
+                          }
+                          return response;
+                      })
+                      .catch(err => {
+                          return cache.match(evt.request);
+                      });
+              }).catch(err => console.log(err))
+          );
+          return;
+      }
+      evt.respondWith(
+          caches.open(CACHE_NAME).then(cache => {
+              return cache.match(evt.request).then(response => {
+                  return response || fetch(evt.request);
+              });
+          })
+      );
+  });
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+// const compression = require("compression");
 
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
-app.use(compression());
+// app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useFindAndModify: false
   
   }, () => 
