74257234076d657db2349b25b3907076900f510d
Burchell <jlkaline@me.com>
2021-05-06 18:05:42 -0400

    port

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
-// const logger = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-// const compression = require("compression");
+const compression = require("compression");
+require ("dotenv").config()
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
-// app.use(compression());
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,16 +20,16 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
+    useCreateIndex: true,
     useFindAndModify: false
   
   }, () => 
-  console.log("Connected to DB"),
-  app.use(require("./routes/api"))
+  console.log("Connected to DB")
+//   app.use(require("./routes/api"))
   );
 
-// routes
-// app.use(require("./routes/api"));
+routes
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
